5ee393828410b169a7583ff5e8353bf6c896e7d0
Charlie B <charlie@charliebatten.co.uk>
2025-08-29 17:55:06 +0100

    Run migrations

---
+++ ArcheryLeageApp.Api/Migrations/20250829164920_NullableLeagueEndDateAndNewCompletedField.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArcheryLeageApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableLeagueEndDateAndNewCompletedField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Leagues",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                table: "Leagues",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                table: "Leagues");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Leagues",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
