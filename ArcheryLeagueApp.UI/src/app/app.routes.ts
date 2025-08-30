9fd0e7a5091d519bf5e8103c0c410b836f6c4d7c
Charlie B <charlie@charliebatten.co.uk>
2025-08-30 16:00:38 +0100

    Add router

--- ArcheryLeagueApp.UI/src/app/app.routes.ts
+++ ArcheryLeagueApp.UI/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PlayersComponent } from './components/players/players.component';
+import { LeaguesComponent } from './components/leagues/leagues.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'leagues', component: LeaguesComponent },
+  { path: 'players', component: PlayersComponent },
+];
