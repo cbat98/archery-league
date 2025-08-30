99fdfe7bfabd61af280346c0b97c12746768284e
Charlie B <charlie@charliebatten.co.uk>
2025-08-30 16:07:55 +0100

    Added default routing

--- ArcheryLeagueApp.UI/src/app/app.routes.ts
+++ ArcheryLeagueApp.UI/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { PlayersComponent } from './components/players/players.component';
 import { LeaguesComponent } from './components/leagues/leagues.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/leagues', pathMatch: 'full' },
   { path: 'leagues', component: LeaguesComponent },
   { path: 'players', component: PlayersComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
